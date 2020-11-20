f4aa5158a61a48f0c0c1d5d71da433c9652edf1b
PdrVinicius <pedro.lopesxavier@gmail.com>
2020-11-19 21:46:23 -0300

    Segue o Projeto 1

--- PedroVinicius_Projeto1_JokenPo.py
+++ PedroVinicius_Projeto1_JokenPo.py
@@ -1,56 +1,92 @@
 from random import randint
 from time import sleep
 
+j1 = 0
+j2 = 0
+c = 0
 lista = ("Pedra", "Papel", "Tesoura")
 
-computador = randint(0,2)
+print(' ')
+jog1 = str(input('Jogador1, Insira Seu Nome: '))
+print(' ')
+jog2 = str(input('Jogador2, Insira Seu Nome: '))
+print(' ')
 
-perguntar = int(input('''Escolha uma opcao para se jogar: 
+while (j1 > 0 or j2 < 2) and (j1 < 2 or j2 > 0) and (c < 3):
+    print(' ')
+    Jogador1 = int(input('''Jogador 1, Escolha uma opcao para se jogar: 
 
-[0] Pedra
-[1] Papel
-[2] Tesoura
+    [0] Pedra
+    [1] Papel
+    [2] Tesoura
  
-Digite sua escolha: '''))
+    Digite sua escolha: '''))
 
-print("JO\n")
-sleep(1)
-print("KEN\n")
-sleep(1)
-print("POOH!!!\n")
+    print(' ')
 
-print("-="*20)
-print("O computador escolheu: {}".format(lista[computador]))
-print("O jogador escolheu: {}".format(lista[perguntar]))
-print("-="*20)
+    Jogador2 = int(input('''Jogador 2, Escolha uma opcao para se jogar: 
 
-if computador == 0:
-    if perguntar == 0:
-        print("Empate!")
-    elif perguntar == 1:
-        print("Jogador perdeu")
-    elif perguntar == 2:
-        print("Computador venceu")
+    [0] Pedra
+    [1] Papel
+    [2] Tesoura
+ 
+    Digite sua escolha: '''))
+
+    print("JO\n")
+    sleep(1)
+    print("KEN\n")
+    sleep(1)
+    print("POOH!!!\n")
+
+    print("-="*20)
+    print(jog2, " escolheu: {}".format(lista[Jogador2]))
+    print(jog1, " escolheu: {}".format(lista[Jogador1]))
+    print("-="*20)
+
+    if Jogador2 == 0:
+        if Jogador1 == 0:
+            print("Empate!")
+        elif Jogador1 == 1:
+            print(jog1, " Venceu, ", jog2, "não foi dessa vez")
+            j1 += 1
+            c += 1
+        elif Jogador1 == 2:
+            print(jog2, " Venceu, ", jog1, "não foi dessa vez")
+            j2 += 1
+            c += 1
+        else:
+            print("Operacao invalida")
+
+    elif Jogador2 == 1:
+        if Jogador1 == 0:
+            print(jog2, " Venceu, ", jog1, "não foi dessa vez")
+            j2 += 1
+            c += 1
+        elif Jogador1 == 1:
+            print("Empate!")
+        elif Jogador1 == 2:
+            print(jog1, " Venceu, ", jog2, "não foi dessa vez")
+            j1 += 1
+            c += 1
+        else:
+            print("Operacao invalida")
+    elif Jogador2 == 2:
+        if Jogador1 == 0:
+            print(jog1, " Venceu, ", jog2, "não foi dessa vez")
+            j1 += 1
+            c += 1
+        elif Jogador1 == 1:
+            print(jog2, " Venceu, ", jog1, "não foi dessa vez")
+            j2 += 1
+            c += 1
+        elif Jogador1 == 2:
+            print("Empate!")
+        else:
+            print("Operacao invalida")
     else:
-        print("Operacao invalida")
-
-elif computador == 1:
-    if perguntar == 0:
-        print("Computador perdeu")
-    elif perguntar == 1:
-        print("Empate!")
-    elif perguntar == 2:
-        print("Jogador venceu")
-    else:
-        print("Operacao invalida")
-elif computador == 2:
-    if perguntar == 0:
-        print("Jogador venceu")
-    elif perguntar == 1:
-        print("Computador venceu")
-    elif perguntar == 2:
-        print("Empate!")
-    else:
-        print("Operacao invalida")
-else:
-    print("Operacao invalida")
+            print("Operacao invalida")
+print(' ')
+if (j1 == 2) and (j1 > j2):
+    print('PARABÉNS ', jog1, 'VOCÊ É O GRANDE VENCEDOR COM ', j1, 'PONTOS!!!')
+elif (j2 == 2) and (j2 > j1):
+    print('PARABÉNS ', jog2, 'VOCÊ É O GRANDE VENCEDOR COM ', j2, 'PONTOS!!!')
